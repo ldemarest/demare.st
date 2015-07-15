903266f036a98b963323f4f9eef4b4f8355f2241
ldemarest <demarest.luke@gmail.com>
2015-07-14 23:06:07 -0400

    removes superfluous if statements

--- to-and-frow.js
+++ to-and-frow.js
@@ -4,18 +4,14 @@ function to (){
 	for(var i = 0; i < 100; i++){
 		var url = Array(i).join("_") + "luke" + Array(100 - i).join("_");
 		window.history.pushState({},'',url);
-		if(i == 99){
-			fro();
-		}
 	}
+	fro();
 }
 
 function fro(){
 	for(var i = 0; i < 100; i++){
 		var url = Array(100 - i).join("_") + "luke" + Array(i).join("_")
 		window.history.pushState({},'',url);
-		if(i == 99){
-			to();
-		}
 	}
+	to();
 }
